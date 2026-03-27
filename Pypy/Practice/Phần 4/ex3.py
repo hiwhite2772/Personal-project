f4ceae25b63fb9e961664c344f7e902977e4d5da
HI White <goldblue123455@gmail.com>
2026-03-27 16:46:13 +0700

    Update 3

--- Pypy/Practice/Phần 4/ex3.py
+++ Pypy/Practice/Phần 4/ex3.py
@@ -1,3 +1,4 @@
+#1
 with open("numbers.txt", "w") as fw:
     fw.write("1\n2\n3\n4\n5\n6")
 
@@ -10,3 +11,23 @@ with open("numbers.txt", "r") as fr:
 
 with open("ketqua.txt", "w") as fw:
     fw.write(str(tong))
+
+#2
+with open("data.txt", "w") as fw:
+    fw.write("10 15 22 33 40 55")
+
+with open("data.txt", "r") as fr:
+    data = list(map(int, fr.read().strip().split()))
+    print(data)
+
+tong = 0
+for line in data:
+    n = int(line)
+    if n % 5 == 0:
+        tong += n
+
+with open("output.txt", "w") as fw:
+    fw.write(str(tong))
+
+with open("output.txt", "r") as fr:
+    print(tong)
