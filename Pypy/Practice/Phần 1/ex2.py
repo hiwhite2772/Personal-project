657d273a62d50a8c67fcca616f04e047ed69e694
HI White <goldblue123455@gmail.com>
2026-03-28 15:46:59 +0700

    Update 4

--- Pypy/Practice/Phần 1/ex2.py
+++ Pypy/Practice/Phần 1/ex2.py
@@ -3,12 +3,15 @@ def nhap_diem(n):
     ds = []
     for i in range(n):
         while True:
-            diem = float(input(f"Nhập điểm thứ {i+1}: "))
-            if 0 <= diem <= 10:
-                ds.append(diem)
-                break
-            else:
-                print("vui lòng nhập điểm từ 0 - 10")
+            try:
+                diem = float(input(f"Nhập điểm thứ {i+1}: "))
+                if 0 <= diem <= 10:
+                    ds.append(diem)
+                    break
+                else:
+                    print("vui lòng nhập điểm từ 0 - 10")
+            except ValueError:
+                print("Vui lòng nhập dữ liệu!")
     return ds
 
 def tinh_tb(ds_diem):
@@ -25,7 +28,15 @@ def xep_loai(dtb):
         return "Yếu"
     
 def main():
-    n = int(input("Nhập số môn học: "))
+    while True:
+        try:
+            n = int(input("Nhập số môn học: "))
+            if n > 0:
+                break
+            print("Vui lòng nhập số dương")
+        except ValueError:
+            print("Vui lòng nhập dữ liệu!")
+
     ds_diem = nhap_diem(n)
     dtb = tinh_tb(ds_diem)
     xl = xep_loai(dtb)
@@ -39,16 +50,29 @@ def fib_de_quy(n):
     if n == 0 or n == 1:
         return n
     return fib_de_quy(n-1) + fib_de_quy(n-2)
-print([fib_de_quy(i) for i in range(11)])
 
 #Khử đệ quy
 def khu_dq(n):
-    if n == 0:
-        return 0
+    if n == 0 or n == 1:
+        return n
+    
     a, b = 0, 1
+
     for _ in range(2, n+1):
         a, b = b, a+b
     return b
-print([khu_dq(i) for i in range(11)])
 
-print(f"n = 10 -> Đệ quy: {fib_de_quy(10)}. Khử DDQ: {khu_dq(10)}")
+def main():
+    while True:
+        try:
+            n = int(input("Nhập số nguyên: "))
+            if n >= 0:
+                break
+            print("Vui lòng nhập số dương!")
+            
+        except ValueError:
+            print("Vui lòng nhập dữ liệu!")
+
+    print(f"n = {n}\n-> Đệ quy: {fib_de_quy(n)}\n-> Khử DDQ: {khu_dq(n)}")
+        
+main()
