580f780eb9a2fff57d795ee23ab5d7014e541167
HI White <goldblue123455@gmail.com>
2026-06-06 21:06:27 +0700

    Update 32

--- Pypy/Ca_nhan/Test/test3_1.py
+++ Pypy/Ca_nhan/Test/test3_1.py
@@ -57,3 +57,16 @@ for i in range(1, n+1):
     print(k*(n-i+1))
 for i in range(1, n+1):
     print(" "*(i-1)+k*(n-i+1))
+
+#11
+n = int(input())
+for i in range(n):
+    print(" "*(n-i-1), end="")
+    if i == 0:
+        print("*")
+    elif i == n-1:
+        print("*"*(2*n-1))
+    else:
+        print("*", end="")
+        print(" "*(2*i-1),end="")
+        print("*")
