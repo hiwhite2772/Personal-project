e2efd00b93551f1b60832f8c7d61647f026513f8
HI White <goldblue123455@gmail.com>
2026-07-12 08:46:34 +0700

    Update 41

---
+++ Cppx/Ca_nhan/B10.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    for (int i = 1; i <= 10; i++) {
+        for (int j = 10; j >= 1; j--) {
+            cout << i << " x " << j << " = " << i*j << "\n";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
